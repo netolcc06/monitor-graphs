31f7b213ef907adbdf8ec6c036bfb4584a2926e2
José Neto <netolcc06@gmail.com>
2024-06-25 15:59:27 -0300

    Add files via upload

---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { RegressionTableComponent } from './components/regression-table/regression-table.component';
+// import { TestGraphComponent } from './components/test-graph/test-graph.component';
+import { AppComponent } from './app.component';
+import { PerfPageComponent } from './components/perf-page/perf-page.component';
+
+export const routes: Routes = [
+    {path : "cvc5-perf", component: PerfPageComponent},
+    {path : "cvc5-regression", component: RegressionTableComponent},
+    {path : "**", component: AppComponent},
+];
